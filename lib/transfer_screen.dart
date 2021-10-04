df6e962e4e41e6eece843862c944bdfaffa7adc6
Marc Pacaldo <fatafatafata322@gmail.com>
2021-10-04 23:12:54 +0800

    Update transfer_screen.dart
    added a stateful widget that the main application instantiates.

--- lib/transfer_screen.dart
+++ lib/transfer_screen.dart
@@ -11,4 +11,19 @@ class TransferScreen extends StatelessWidget {
 
   static const String _title = 'Flutter Code Sample';
 
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
 
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
