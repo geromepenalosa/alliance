89792646ecf9fadd8eab29d95486d79add1e5630
Marc Pacaldo <fatafatafata322@gmail.com>
2021-10-04 23:04:54 +0800

    Added a Transfer Screen page

---
+++ lib/transfer_screen.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+/// This is the main application widget.
+class TransferScreen extends StatelessWidget {
+  const TransferScreen({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
+
