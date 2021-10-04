1a3d7094837fd3dee1f070b446edb4d103d79d24
Marc Pacaldo <fatafatafata322@gmail.com>
2021-10-04 23:29:08 +0800

    Update transfer_screen.dart
    Added a navigationrail widget

--- lib/transfer_screen.dart
+++ lib/transfer_screen.dart
@@ -27,3 +27,70 @@ class MyStatefulWidget extends StatefulWidget {
   @override
   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  bool _extended = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Row(
+          children: <Widget>[
+            NavigationRail(
+              selectedIndex: _selectedIndex,
+              leading: ElevatedButton(
+                onPressed: () {},
+                child: Icon(Icons.person, color: Colors.black),
+                style: ElevatedButton.styleFrom(
+                  shape: CircleBorder(),
+                  padding: EdgeInsets.all(5),
+                  primary: Color(0xffFFC835),
+                ),
+              ),
+              backgroundColor: Color(0xff191A22),
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              labelType: NavigationRailLabelType.selected,
+              destinations: const <NavigationRailDestination>[
+                NavigationRailDestination(
+                  icon: Icon(
+                    Icons.holiday_village_outlined,
+                    color: Color(0xffF5F8FA),
+                  ),
+                  selectedIcon: Icon(
+                    Icons.holiday_village,
+                    color: Color(0xffFFC835),
+                  ),
+                  label: Text(
+                    'Dashboard',
+                    style: TextStyle(color: Color(0xffFFC835), fontSize: 10),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                NavigationRailDestination(
+                    icon: Icon(Icons.attach_money, color: Color(0xffF5F8FA)),
+                    selectedIcon:
+                        Icon(Icons.attach_money, color: Color(0xffFFC835)),
+                    label: Text(
+                      'Transfer\nMoney',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Color(0xffFFC835), fontSize: 10),
+                    )),
+                NavigationRailDestination(
+                  padding: EdgeInsets.only(top: 350),
+                  icon: Icon(Icons.settings_outlined, color: Color(0xffF5F8FA)),
+                  selectedIcon: Icon(
+                    Icons.settings,
+                    color: Color(0xffFFC835),
+                  ),
+                  label: Text('Account\nSettings',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Color(0xffFFC835), fontSize: 10)),
+                ),
+              ],
+            ),
+            const VerticalDivider(thickness: 1, width: 4),
